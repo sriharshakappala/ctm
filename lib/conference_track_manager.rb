8187a1a5b9c55451cc56adf8d15e3b9b1651a432
Sri Harsha Kappala <sriharsha.kappala@hotmail.com>
2016-03-29 20:03:10 +0530

    Loading file path from env and base skeleton for CTM file

--- lib/conference_track_manager.rb
+++ lib/conference_track_manager.rb
@@ -1,5 +1,23 @@
-require_relative 'talk'
-require_relative 'session'
+require_relative './ctm/talk'
+require_relative './ctm/session'
+require 'pry'
+require 'yaml'
 
-class ConferenceTrackManager
+def init_talks_list file
+  binding.pry
+end
+
+def plan_talks_for_tracks talks_list
+end
+
+environment = YAML.load_file("../env.yml")
+
+if ARGV[0].nil?
+  puts "Usage: `ruby conference_track_manager.rb <file_name>`"
+  puts "<file_name> must be any of the existing files from the `inputs` directory"
+  raise 'NoFileGivenException'
+else
+  file = ARGV[0]
+  binding.pry
+  talks_list = init_talks_list environment["inputs_directory"] + file
 end
