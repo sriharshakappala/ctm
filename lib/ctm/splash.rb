0b119d47c7cf7e5e1d120101f0af4dd011c87a16
Sri Harsha Kappala <sriharsha.kappala@hotmail.com>
2016-03-30 14:43:10 +0530

    Track end fixed

--- lib/ctm/splash.rb
+++ lib/ctm/splash.rb
@@ -6,6 +6,7 @@ class Splash
   def self.display_plan tracks, track_count
     track_count.times do |counter|
       puts "Track #{counter + 1}"
+      puts "*************"
       splash tracks[(counter*2)]
       splash tracks[(counter*2) + 1]
     end
@@ -18,6 +19,11 @@ class Splash
     track.talks.each_with_index do |talk, index|
       start_time = index == 0 ? start_time : get_time(start_time, talk.length)
       puts "#{start_time} #{talk.description}"
+    end
+    if track.type == :morning
+      puts "12:00PM Lunch"
+    else
+      puts "05:00PM Networking Event"
     end
   end
 
