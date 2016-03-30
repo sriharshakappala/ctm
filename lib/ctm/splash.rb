9fb0144d7741d2bdcee7c6fefe89003af681050a
Sri Harsha Kappala <sriharsha.kappala@hotmail.com>
2016-03-30 14:29:25 +0530

    Added

--- lib/ctm/splash.rb
+++ lib/ctm/splash.rb
@@ -6,21 +6,23 @@ class Splash
   def self.display_plan tracks, track_count
     track_count.times do |counter|
       puts "Track #{counter + 1}"
-      splash tracks[counter]
-      splash tracks[counter+2]
+      splash tracks[(counter*2)]
+      splash tracks[(counter*2) + 1]
     end
   end
 
   private
 
   def self.splash track
-    binding.pry
     start_time = track.type == :morning ? Time.new(2016,1,1, 9,00).strftime('%I:%M%p') : Time.new(2016,1,1, 13,00).strftime('%I:%M%p')
-    track.talks.each do |talk|
+    track.talks.each_with_index do |talk, index|
+      start_time = index == 0 ? start_time : get_time(start_time, talk.length)
       puts "#{start_time} #{talk.description}"
-      start_time = Time.parse(start_time) + track.length * 60
-      start_time.strftime('%I:%M%p')
     end
+  end
+
+  def self.get_time start_time, length
+    (Time.parse(start_time) + length * 60).strftime('%I:%M%p')
   end
 
 end
