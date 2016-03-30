59c86e0f05f686096070dd8feb423a40e71660b9
Sri Harsha Kappala <sriharsha.kappala@hotmail.com>
2016-03-30 19:10:12 +0530

    Added more unit tests to session_spec

--- spec/ctm/session_spec.rb
+++ spec/ctm/session_spec.rb
@@ -16,4 +16,36 @@ RSpec.describe Session do
     expect { session.add talk }.to raise_error 'TalkTooLongException'
   end
 
+  it "must return true when checking whether a talk can be added into session when there is enough time" do
+    session = Session.new 50, :morning
+    talk = Talk.new 'Example Talk 10min'
+    expect(session.add? talk).to eq true
+  end
+
+  it "must return false when checking whether a talk can be added into session when there is no enough time" do
+    session = Session.new 50, :morning
+    talk = Talk.new 'Example Talk 60min'
+    expect(session.add? talk).to eq false
+  end
+
+  it "will not add a second talk if the available time is zero or less than the second talk length" do
+    session = Session.new 50, :morning
+    talk1 = Talk.new 'Example Talk 30min'
+    talk2 = Talk.new 'Example Talk 2 30min'
+    session.add talk1
+    expect(session.talks).to include talk1
+    expect { session.add talk2 }.to raise_error 'TalkTooLongException'
+  end
+
+  it "must have accurate availabe time after adding talks to session" do
+    session = Session.new 50, :morning
+    talk1 = Talk.new 'Example Talk 10min'
+    talk2 = Talk.new 'Example Talk 2 20min'
+    session.add talk1
+    session.add talk2
+    expect(session.talks).to include talk1
+    expect(session.talks).to include talk2
+    expect(session.length).to eq 20
+  end
+
 end
